d93f9db1dab5404f122d7e84bea85b3d2d057ab8
Nafis Abdullah Ahmad <n.ahmad.web.cit22@gmail.com>
2024-06-13 12:32:08 +0600

    update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use App\Classes\CLIApplication;
 
-require 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 // Run the application
 $app = new CLIApplication();
